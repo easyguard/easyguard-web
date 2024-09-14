56f5834cbe6240038abe7465f904438a7eef0cf5
Cfp <chaosflo44plus@gmail.com>
2024-09-14 15:57:33 +0200

    📝 Add beta dialog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,31 @@
 <script>
 	import "$lib/style.css";
-    import Header from "./Header.svelte";
-    import Nav from "../lib/nav/Nav.svelte";
+	import Header from "./Header.svelte";
+	import Nav from "../lib/nav/Nav.svelte";
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		const dialog = document.getElementById("dialog");
+		// @ts-ignore
+		dialog.showModal();
+	})
 </script>
+
+<dialog id="dialog" style="border: none; border-radius: 15px; background-color: #1d1d1d; color: white; outline: none;">
+	<h2>Willkommen bei EasyGuard 2.0 BETA</h2>
+	<p>EasyGuard 2.0 ist noch in der Entwicklung und kann Fehler enthalten.</p>
+	<p>Bitte nicht als einzigen Router produktiv einsetzen, sonst knallt es irgendwann und du bist traurig.</p>
+	<p>Funktionierende Funktionen dieser funktionalen Software:</p>
+	<pre>Firewall (Limes) - EZGDISP (S1 Display) - DNS Blocker (Blocky, noch nicht einstellbar!) - DHCP (ebenfalls nicht einstellbar)</pre>
+	<p>Wichtige, noch fehlende Funktionen (ausschnitt):</p>
+	<pre>WireGuard - Geräte mit MacID (Mac Address Info) - EasyGuard Controller - OOBE</pre>
+
+	<button on:click={() => {
+		const dialog = document.getElementById("dialog");
+		// @ts-ignore
+		dialog.close();
+	}}>Verstanden</button>
+</dialog>
 
 <div class="app">
 	<Header />
@@ -22,5 +45,8 @@
 		display: flex;
 		flex-direction: column;
 		gap: 15px;
+	}
+	#dialog::backdrop {
+		background-color: rgba(0, 0, 0, 0.5);
 	}
 </style>
